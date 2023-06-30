52117f80346a22732b7ca68a7deabde9fd2c8e66
Jesse Baltazar <jmb11@acuitysso.com>
2023-06-29 19:54:02 -0600

    Updated

--- async.py
+++ async.py
@@ -4,9 +4,19 @@ from threading import Timer
 
 relay = LED(19)
 
-def hello():
+SW_R = Button(5)
+SW_G = Button(6)
+SW_Y = Button(13)
+
+R = LED(17)
+G = LED(27)
+B = LED(22)
+
+def relay_on():
     print("On")
     relay.on()
+   # apply_source(SW_R,R)
+
     #   start timer
     timer = Timer(5, relay_off, args=())
     timer.start()
@@ -15,12 +25,32 @@ def relay_off():
     print("Off")
     relay.off()
 
-button_on = Button(2)
-button_on.when_pressed = hello
+def apply_source(SWITCH_INPUT, LED_OUTPUT):
+    LED_OUTPUT.source = SWITCH_INPUT
 
 
-button_off = Button(3)
-button_off.when_pressed = relay_off
+
+def led_on(L1):
+    print("led_on")
+    L1.on()
+
+def x():
+    print("Button 5 pressed")
+def y():
+    print("Button 6 pressed")
+def z():
+    print("Button 13 pressed")
+
+
+SW_R.when_pressed = x
+SW_G.when_pressed = y
+SW_Y.when_pressed = z
+
+trigger = Button(2)
+trigger.when_pressed = relay_on
+
+resetbutton = Button(3)
+resetbutton.when_pressed = relay_off
 
 
 pause()
